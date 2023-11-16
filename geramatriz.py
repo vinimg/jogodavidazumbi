67fb9da7d2990cb5b7d9a61c8d220eb9107838b1
vinimg <viniciusmg@id.uff.br>
2023-11-16 14:39:25 -0300

    gera

--- geramatriz.py
+++ geramatriz.py
@@ -1,7 +1,7 @@
 import random
 
 def generate_matrix_file(filepath, rows, cols):
-    matrix = [[random.randint(0, 2) for j in range(cols)] for i in range(rows)]
+    matrix = [[random.choices([0, 1, 2], weights=[1, 2, 1], k=1)[0] for j in range(cols)] for i in range(rows)]
     with open(filepath, 'w') as f:
         for row in matrix:
             f.write(' '.join(map(str, row)) + '\n')
